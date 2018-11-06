88ad14c829c9f7f8ddfac3fd07a0c2f12b83a52d
Nathan Nolte <nnolte@fvlhs.org>
2018-11-06 19:00:50 +0000

    Finished testing

--- team3.py
+++ team3.py
@@ -6,29 +6,44 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Julian'
+strategy_name = 'Two attacks, betray. Two collusions, collude.'
+strategy_description = 'Two strikes and I reltaliate.'
     
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+def move(my_history,their_history, my_score,their_score):
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+   
 
+    prob_oppo_b = 0
+    prob_oppo_c = 0
     
+    if len(my_history)== 0:
+        return 'b'
+
+    elif their_history[-1] == 'c': 
+        prob_oppo_c += 1 #Add 1 to the times they have colluded.
+        return 'c' #Collude
+            
+    elif their_history[-1] == 'b': #If they have betrayed you once, betray them back.
+        return 'b' #Betray
+        
+    elif their_history[-3,-2,-1] == 'c':    #If they have colluded for the past 3 turns, betray them.
+        return 'b' #Betray
+    
+    elif their_history[-1] =='b' and their_history[-2] =='c':
+        prob_oppo_b +=1        
+        return 'b'
+            
+    elif  their_history[-2] =='b' and their_history[-1] =='b': #If they have 
+        
+        return 'b' #Betray
+        prob_oppo_b +=1 #Add one to the likelyhood that they will betray you. 
+        
+    if prob_oppo_b >= 3:
+    
+     return 'b' #Betray
+     prob_oppo_b = 0 #Reset the likelyhood factor.
+     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
